e16f04dabbc0432570ba405a1dc45fd7c2ff1daf
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-24 00:48:04 +0000

    Week 5 fix up

---
+++ portfolio/week5/UserAccounts.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#Function to print an Underline of a certain Length.
+UnderLine() {
+	string=$(printf "%0.s_" $(seq 1 $1))
+	printf "$string"
+}
+
+#Function to print whole line for the table.
+WholeLine() {
+	printf "$1"
+	UnderLine 22
+	printf "$1"
+	UnderLine 10
+	printf "$1"
+	UnderLine 10
+	printf "$1"
+	UnderLine 35
+	printf "$1"
+	UnderLine 22
+	printf "$1\n"
+}
+
+AR="\x1b[32m"
+AN="\x1b[0m"
+
+#Colours
+R="\033[1;31m"
+G="\033[1;32m"
+Y="\033[1;33m"
+B="\033[1;34m"
+P="\033[1;35m"
+N="\033[0m"
+
+#Variables
+SPACE=" "
+VERTICAL="|"
+
+#cc $R "test" $N
+
+#Prints top vertical Line of table
+WholeLine "$SPACE"
+
+#Prints table headings
+printf "| $B%-20s$N | $B%-8s$N | $B%-8s$N | $B%-33s$N | $B%-20s$N |\n" "Username" "UserID" "GroupID" "Home" "Shell"
+
+#Prints Vertical line of table with `|`
+WholeLine "$VERTICAL"
+
+awk -v r=$R -v g=$G -v y=$Y -v p=$P -v n=$N -F: '{printf "| %s%-20s%s | %s%-8s%s | %s%-8s%s | %s%-33s%s | %-20s |\n", r,$1,n,g,$3,n,y,$4,n,p,$6,n,$7}' /etc/passwd
+
+
+#Prints Bottom line of table.
+WholeLine "$VERTICAL"
+
+
+
+
+
+
+
+
+
